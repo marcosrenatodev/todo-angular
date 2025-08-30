d142dfccbd4f6927c132ee2a7d8d7798c427043b
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2025-08-30 16:25:29 -0300

    First commit
    Initializing repository.
    Created a few components and a TodoService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  <app-header/>
+  <main>
+    <router-outlet/>
+  </main>
   `,
-  styles: [],
-})
-export class AppComponent {
-  title = 'first-ng-app';
-}
+  styles: [`
+    main {
+      padding: 16px
+    }
+    `],
+  })
+  export class AppComponent {
+    title = 'todo-angular';
+  }
