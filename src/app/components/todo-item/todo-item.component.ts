90e4a76e3206c4b6cbbd04f10c822dbbac4a860a
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2025-08-30 17:25:24 -0300

    Angular pipes

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlighCompletedTodoDirective } from '../../directives/highligh-completed-todo.directive';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [HighlighCompletedTodoDirective],
+  imports: [HighlighCompletedTodoDirective, UpperCasePipe],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
